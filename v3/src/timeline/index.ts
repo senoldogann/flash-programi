f58fcdc37293d0c8ccd233aa18e6d206507fc4a1
Senol Dogan <s.dogann@yandex.com>
2026-09-09 22:23:51 +0300

    feat: expose Project V3 timeline core APIs

---
+++ v3/src/timeline/index.ts
@@ -0,0 +1,10 @@
+export { evaluateClip, type EvaluatedClipV3 } from './clip-evaluator';
+export { composeClipEvaluations, type ComposedAnimationV3 } from './composition';
+export { applyEasing } from './easing';
+export {
+  evaluateScene,
+  type ResolvedAnimationChannelsV3,
+  type ResolvedLayerTransformV3,
+  type ResolvedLayerV3,
+  type ResolvedSceneV3,
+} from './scene-evaluator';
