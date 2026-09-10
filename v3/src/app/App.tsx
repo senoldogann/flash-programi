a48340695c9f87bc598d48d2048db1d9d6cb9347
Senol Dogan <s.dogann@yandex.com>
2026-09-10 22:04:49 +0300

    style: load comfortable Turkish editor overrides

--- v3/src/app/App.tsx
+++ v3/src/app/App.tsx
@@ -2,6 +2,7 @@ import { EditorShell } from '../editor/EditorShell';
 import '../styles.css';
 import '../responsive.css';
 import '../editor/editor-workflow.css';
+import '../editor/easy-ui.css';
 
 export function App() {
   return <EditorShell />;
