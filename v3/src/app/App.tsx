c8b80f68f4bcee33fe7d95682fabf548093f3c7b
Senol Dogan <s.dogann@yandex.com>
2026-09-10 22:41:10 +0300

    chore: sync 2026 shell styles import

--- v3/src/app/App.tsx
+++ v3/src/app/App.tsx
@@ -3,6 +3,7 @@ import '../styles.css';
 import '../responsive.css';
 import '../editor/editor-workflow.css';
 import '../editor/easy-ui.css';
+import '../editor/editor-2026.css';
 
 export function App() {
   return <EditorShell />;
